12493addfb2e747ca36737cce385fbb7db962f6c
Adam Gerber <adam.gerber@yahoo.com>
2015-01-27 15:47:44 -0600

    Delegates handling of spinners' behavior to MainActivity

--- app/src/main/java/com/apress/gerber/currencies/MainActivity.java
+++ app/src/main/java/com/apress/gerber/currencies/MainActivity.java
@@ -9,6 +9,8 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
@@ -19,7 +21,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends ActionBarActivity implements AdapterView.OnItemSelectedListener {
 
     //define members that correspond to Views in our layout
     private Button mCalcButton;
@@ -64,6 +66,10 @@ public class MainActivity extends ActionBarActivity {
         //assign adapters to spinners
         mHomSpinner.setAdapter(arrayAdapter);
         mForSpinner.setAdapter(arrayAdapter);
+
+        mHomSpinner.setOnItemSelectedListener(this);
+        mForSpinner.setOnItemSelectedListener(this);
+
 
     }
 
@@ -133,4 +139,28 @@ public class MainActivity extends ActionBarActivity {
 
     }
 
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+        switch (parent.getId()) {
+
+            case R.id.spn_for:
+                //define behavior here
+                break;
+
+            case R.id.spn_hom:
+                //define behavior here
+                break;
+
+            default:
+                break;
+        }
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
+    }
 }
